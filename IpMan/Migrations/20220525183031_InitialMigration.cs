52c4fae2761b4d236e95787d91951560244ac89e
Piotr Kowalski <piotr@pkowalski.pw>
2022-05-25 20:31:40 +0200

    Add initial migration

---
+++ IpMan/Migrations/20220525183031_InitialMigration.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IpMan.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Administrators",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    EmailAddress = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Administrators", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Buildings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    AddressLine1 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    AddressLine2 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buildings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RackSpaces",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Room = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    ServerRack = table.Column<int>(type: "integer", nullable: false),
+                    RackRow = table.Column<int>(type: "integer", nullable: false),
+                    BuildingId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RackSpaces", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RackSpaces_Buildings_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Buildings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Servers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Ip = table.Column<string>(type: "character varying(45)", nullable: false),
+                    RackSpaceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AdministratorId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Servers_Administrators_AdministratorId",
+                        column: x => x.AdministratorId,
+                        principalTable: "Administrators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Servers_RackSpaces_RackSpaceId",
+                        column: x => x.RackSpaceId,
+                        principalTable: "RackSpaces",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buildings_Name",
+                table: "Buildings",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RackSpaces_BuildingId",
+                table: "RackSpaces",
+                column: "BuildingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Servers_AdministratorId",
+                table: "Servers",
+                column: "AdministratorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Servers_Ip",
+                table: "Servers",
+                column: "Ip",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Servers_RackSpaceId",
+                table: "Servers",
+                column: "RackSpaceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Servers");
+
+            migrationBuilder.DropTable(
+                name: "Administrators");
+
+            migrationBuilder.DropTable(
+                name: "RackSpaces");
+
+            migrationBuilder.DropTable(
+                name: "Buildings");
+        }
+    }
+}
